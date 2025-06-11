88ac7cd0dc228fbdca1d39d776057736432c8d96
Benoit Daumas <frosterno@yahoo.fr>
2025-06-11 15:54:18 +0200

    commit benoit registry

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 
 """
 
-from titanic.data import load_data,clean_data,prepare_data
-from titanic.registry import save_model
-from titanic.train import train_model, evaluate_model, optimize_model
+from src.titanic.data import load_data,clean_data,prepare_data
+from src.titanic.registry import save_model
+from src.titanic.train import train_model, evaluate_model, optimize_model
+from src.titanic.utils import hello_world
+
+print(hello_world())
