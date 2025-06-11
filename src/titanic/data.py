f4fb507e8926f813b4e7d1f769ef06673ffb723a
Guillaume Barral <bar.gui@wanadoo.fr>
2025-06-11 16:14:19 +0200

    ajout load and prepare data

--- src/titanic/data.py
+++ src/titanic/data.py
@@ -1,9 +1,11 @@
 """
 Load, preprocess, prepare, and save the Titanic dataset.
 """
-
+import os
 import pandas as pd
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
 
 
 def load_data():
@@ -13,7 +15,20 @@ def load_data():
     Returns:
         DataFrame: The loaded Titanic dataset.
     """
-    pass
+    DATA_DIR = "data/"
+
+    train_df = pd.read_csv(os.path.join(DATA_DIR,"train.csv"),index_col=0)
+
+    test_df_missing_target  = pd.read_csv(os.path.join(DATA_DIR,"test.csv"),index_col=0)
+    target_test_df          = pd.read_csv(os.path.join(DATA_DIR,"gender_submission.csv"),index_col=0)
+    test_df = pd.merge(  test_df_missing_target
+                       , target_test_df
+                       , left_index=True
+                       , right_index=True
+                       , how='left'
+                       , suffixes=('', '_target')).copy()
+    
+    return train_df, test_df
        
 def clean_data(df):
     """
@@ -46,4 +61,19 @@ def prepare_data(df:pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
     Returns:
         tuple: A tuple containing [X,y] the features DataFrame and the target Series.
     """
-    pass
+
+    numeric_features = ['Age', 'Fare']
+
+    scaler = StandardScaler()
+    df_scaled = df.copy()
+    df_scaled[numeric_features] = scaler.fit_transform(df[numeric_features])
+
+    categorical_features =  ["Sex", "Embarked"]
+    encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore', drop='first').set_output(transform="pandas")
+    df_encoded = encoder.fit_transform(df[categorical_features])
+    
+    df_final = pd.concat([df_scaled, df_encoded], axis=1).drop(columns=['Sex', 'Embarked'])
+
+    X = df_final.drop(columns=['Survived'])
+    y = df_final['Survived']
+    return (X, y)
